180580759f46cb83f2a81c38a03d8ba15e87bbff
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-11-21 11:21:05 -0600

    Fixed Margin and Sizing

--- src/components/ExperienceSection/Experience.tsx
+++ src/components/ExperienceSection/Experience.tsx
@@ -4,7 +4,7 @@ function Experience() {
   return (
     <div className="Experience">
       <div className="reveal">
-        <h1 id="experience-header">Experience</h1>
+        <h1 id="experience-header">Work Experience</h1>
       </div>
       <div id="full-experience-container">
         <div id="experience-graph-container">
@@ -15,27 +15,35 @@ function Experience() {
               <div className="reveal gc graph-circle"></div>
               <Dots />
               <div className="reveal gc graph-circle"></div>
+              <Dots />
+              <div className="reveal gc graph-circle"></div>
             </div>
           </div>
           <div id="experience-info">
-            <div className="reveal" id="info">
+            <div className="info reveal">
               <h2>Daily Printing</h2>
               <p>2021 - Current</p>
               <p>HTML, CSS, JavaScript, Python</p>
             </div>
             <br />
             <br />
-            <div className="reveal" id="info">
+            <div className="info reveal">
               <h2>Online Tutoring</h2>
               <p>2019 - 2021</p>
               <p>HTML, CSS, JavaScript</p>
             </div>
             <br />
             <br />
-            <div className="reveal" id="info">
+            <div className="info reveal">
               <h2>Anoka Technical College</h2>
               <p>2019 - 2021</p>
               <p>HTML, CSS, JavaScript, Python</p>
+            </div>
+            <br />
+            <br />
+            <div className="info reveal">
+              <h2>US Army (25Q)</h2>
+              <p>2015 - 2019</p>
             </div>
           </div>
         </div>
