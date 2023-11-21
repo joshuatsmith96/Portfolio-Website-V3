bfc8d2a9b61b1fda632c6340a70b52d251932ffd
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-11-21 10:26:07 -0600

    Created Mobile Contact Form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,22 @@
 function ContactForm(){
     return(
         <div className="ContactForm">
-            <input type="text" />
+            <h2>Want to chat?</h2>
+            <p>Fill out the form below!</p>
+            <div className="name-inputs">
+                <input type="text" placeholder="First Name"/>
+                <input type="text" placeholder="Last Name"/>
+            </div>
+            <div className="name-inputs">
+            <input type="text" placeholder="Email"/>
+            </div>
+            <div className="name-inputs">
+            <input type="text" placeholder="Subject"/>
+            </div>
+            <textarea />
+            <div className="center">
+            <button>Send Message</button>
+            </div>
         </div>
     )
 }
