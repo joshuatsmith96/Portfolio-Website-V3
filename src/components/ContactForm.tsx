6b2fe72c54e1a171bf875a89e7136d46180aa8f8
Josh Smith <jsmith@dailyprinting.local>
2023-12-27 14:33:09 -0600

    Contact form functional
    The contact form is now functional. It uses EmalJS to send emails to my gmail.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,22 +1,48 @@
-function ContactForm(){
-    return(
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+
+function ContactForm() {
+
+    const form: any = useRef();
+
+    const sendEmail = () => {
+        emailjs
+            .sendForm(
+                "service_ig752d9",
+                "template_yybutzh",
+                form.current!,
+                "SFLxiJQnkzg28apJM"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
+
+    return (
         <div className="ContactForm">
             <h2>Want to chat?</h2>
             <p>Fill out the form below!</p>
-            <div className="name-inputs">
-                <input type="text" placeholder="First Name"/>
-                <input type="text" placeholder="Last Name"/>
-            </div>
-            <div className="name-inputs">
-            <input type="text" placeholder="Email"/>
-            </div>
-            <div className="name-inputs">
-            <input type="text" placeholder="Subject"/>
-            </div>
-            <textarea placeholder="Enter Message Here" />
-            <div className="center">
-            <button>Send Message</button>
-            </div>
+            <form ref={form} onSubmit={sendEmail}>
+                <div className="name-inputs">
+                    <input type="text" placeholder="Name" name="user_name" />
+                </div>
+                <div className="name-inputs">
+                    <input type="text" placeholder="Email" name="user_email" />
+                </div>
+                <div className="name-inputs">
+                    <input type="text" placeholder="Subject" name="user_subject" />
+                </div>
+                <textarea placeholder="Enter Message Here" name="message" />
+                <div className="center">
+                    <button type="submit">Send Message</button>
+                </div>
+            </form>
         </div>
     )
 }
