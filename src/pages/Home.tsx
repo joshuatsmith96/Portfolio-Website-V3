e82d68520ee41ec3aeca200646a6195fb9cb9e80
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2025-02-23 15:50:33 -0600

    Removing unused code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@
 import '../App.css'
 import "../PageScrollStyles.css"
 //Scripts
-import { useEffect, useState } from 'react'
 import { Parallax, Background } from 'react-parallax'
 //Sections
 // import HeroSection from '../components/HeroSection/HeroSection'
@@ -18,45 +17,6 @@ import ProjectSection from '../components/ProjectSection/ProjectSection.tsx'
 import BackgroundImageMobile from '../assets/Images/Background V3.jpg'
 
 function Home() {
-
-
-  //calling api
-const [list, setList] = useState([])
-
-//Seperate List Data Into Useable Segments
-
-useEffect(() => {
-    fetch('http://localhost:1337/api/landing-pages')
-    .then((response) => response.json())
-    .then((data) => doSomething(data))
-}, [])
-
-const doSomething =(data:[]) => {
-  console.log(data)
-}
-
-//end calling api
-
-  // function isInViewport(element:any) {
-  //   var rect = element.getBoundingClientRect();
-  //   var html = document.documentElement;
-  //   return (
-  //     rect.top >= 0 &&
-  //     rect.left >= 0 &&
-  //     rect.bottom <= (window.innerHeight || html.clientHeight) &&
-  //     rect.right <= (window.innerWidth || html.clientWidth)
-  //   );
-  // }
-
-  // let image = document.getElementById("profile-image")
-
-  // document.addEventListener("scroll", (event:any) => {
-  //   if(isInViewport(image) === true){
-  //     console.log("In View")
-  //   } else {
-  //     console.log("Not In View")
-  //   }
-  // })
 
   return (
     <div className='Home'>
